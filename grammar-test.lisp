8499e8bb2e480ab32c1afd0fabe9b9d6489f7e5b
Timothy <t.williams.im@gmail.com>
2015-07-12 01:37:31 -0400

    update

--- grammar-test.lisp
+++ grammar-test.lisp
@@ -7,32 +7,9 @@
 
 (in-package #:grammar-test)
 
-(define-test square-1
-  (assert-equal 25 (grammar:square 5)))
+(define-test foo-1
+  (assert-equal 25 (grammar:foo 5)))
 
-;; (define-test square-1
-;;   (assert-equal 1 (grammar:square 1)))
-
-;; (define-test square-2
-;;   (assert-equal 2 (grammar:square 2)))
-
-;; (define-test square-3
-;;   (assert-equal 4 (grammar:square 3)))
-
-;; (define-test square-4
-;;   (assert-equal 8 (grammar:square 4)))
-
-;; (define-test square-16
-;;   (assert-equal 32768 (grammar:square 16)))
-
-;; (define-test square-32
-;;   (assert-equal 2147483648 (grammar:square 32)))
-
-;; (define-test square-64
-;;   (assert-equal 9223372036854775808 (grammar:square 64)))
-
-;; (define-test total-grammar
-;;   (assert-equal 18446744073709551615  (grammar:total)))
 
 #-xlisp-test
 (let ((*print-errors* t)
